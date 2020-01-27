2f115a46d25885eb0e67d618bbd347344893cbd3
Catherine Kim <catherinesjkim@gmail.com>
2020-01-26 18:30:32 -0800

    Add Updated App js file Because: - Added a class component that fetches data from the server I now have running
    This commit:
    - The data was is fetched from Google Trends API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
+// 1. Class component that fetches data from the server you now have running - the data can be fetched from `http://localhost:5000/api/players`
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: []
+      };
+    }
+
+    componentDidMount() {
+      fetch('http://localhost:5000/api/players')
+      .then(response => response.json())
+      .then(data => this.setState({ ...this.state, data: data }))
+    }
+
+    render () {
+      return (
+        <div className="App" data-testide="toggle">
+          
+        </div>
+    );
+  }
 }
 
 export default App;
