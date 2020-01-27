ea54570ccb6c6aa86a70aa3e3126067554f568d8
Catherine Kim <catherinesjkim@gmail.com>
2020-01-26 18:45:30 -0800

    Add Updated App, Navbar, and Players js files Because: - Displayed the player data I receive from the API on the browser/page
    This commit:
    - Added a constructor with state to initialize the data/state
    - Added a keyword for class components for classes that I need to call its constructor
    - A component lifecycle method was added to initiate a fetch from API and setup an event listener
    - Added a navbar and players components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 // 1. Class component that fetches data from the server you now have running - the data can be fetched from `http://localhost:5000/api/players`
+// 2. Display the player data you receive from the API
 
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Players from './components/Players';
 
 class App extends React.Component {
     constructor(props) {
@@ -20,7 +23,22 @@ class App extends React.Component {
     render () {
       return (
         <div className="App" data-testide="toggle">
-          
+          <Navbar />
+          <div className="header">
+            <h1>Players</h1>
+          </div>
+          <div>
+            {this.state.data.map(info => {
+
+              return (
+                <Players 
+                name={info.name}
+                country={info.country}
+                searches={info.searches}
+                />
+              )
+            })}
+          </div>
         </div>
     );
   }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Women's World Cup Most Searched players</h1>
+      <div className="secondLine">
+        <h2>By Google Trends</h2>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
